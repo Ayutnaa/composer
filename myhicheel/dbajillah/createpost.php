c47c883c79566105fb04803449fa778f0def69fb
Ayutnaa <curlets1123@gmail.com>
2022-11-01 14:12:41 +0800

    sanitize

--- myhicheel/dbajillah/createpost.php
+++ myhicheel/dbajillah/createpost.php
@@ -1,5 +1,14 @@
 <?php
 include 'includes/logincheck.php';
+$beldex = $con->prepare("insert into `posts` (user_id, post_title, post_content) values (?,?,?)");
+$beldex->bind_param("isb", $user_id, $post_title, $post_content);
 
+$user_id = $user['id'];
+$post_title = filter_var($_POST['post_title'], FILTER_SANITIZE_STRING);
+$post_content = filter_var($_POST['post_content'], FILTER_SANITIZE_STRING);
+$results = $beldex->();
 
+if($results) {
+    
+}
 ?>
